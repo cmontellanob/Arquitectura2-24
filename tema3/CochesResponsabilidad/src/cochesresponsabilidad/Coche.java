84c602735a5d678383abcf66c709f192b9b4c052
cmontellano <carlosmontellano@gmail.com>
2024-08-27 11:58:43 -0400

    27-08-24

---
+++ tema3/CochesResponsabilidad/src/cochesresponsabilidad/Coche.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cochesresponsabilidad;
+
+/**
+ *
+ * @author DELL
+ * Declarar la clase coche  que tiene los siguientes propiedades y metodos:
+ * Luego crear unmenu con las opciones crear un coche y Guardar el Coche,
+ * Guardar permitira guardar en una mase de datos mysql que llame be_coches , la tabla 
+ * coches con los campos que sean necesarios
+ */ 
+
+public class Coche {
+    private Marca marca;
+    private int modelo;
+    private String placa;
+    private Color color;
+    private int cilindrada;
+    
+    public void GuardarBd(){
+         
+    }
+    
+}
---
+++ tema3/CochesResponsabilidad/src/cochesresponsabilidad/Color.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package cochesresponsabilidad;
+
+/**
+ *
+ * @author Carlos
+ */
+public enum Color {
+    rojo,azul,amarillo,blanco,verde,celeste,anaranjado,negro
+}
